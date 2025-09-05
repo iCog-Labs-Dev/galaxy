f3196b1e1d5fe9baa1635069dda39a65ec03671d
johnnas12 <yohanesmesfin3@gmail.com>
2025-09-05 17:04:50 +0300

    frontend changes

--- client/src/components/Workflow/WorkflowNavigationTitle.vue
+++ client/src/components/Workflow/WorkflowNavigationTitle.vue
@@ -43,6 +43,12 @@ const emit = defineEmits<{
     (e: "on-execute"): void;
 }>();
 
+function runWorkflowAndRedirect() {
+    emit('on-execute');
+    window.open("http://100.67.47.42:3001", "_blank");
+}
+
+
 const { workflow, loading, error, owned } = useWorkflowInstance(props.workflowId);
 
 const { isAnonymous } = storeToRefs(useUserStore());
@@ -181,7 +187,7 @@ async function rerunWorkflow() {
                         size="small"
                         :tooltip="executeButtonTooltip"
                         :title="!props.validRerun ? localize('Run Workflow') : localize('Rerun Workflow')"
-                        @onClick="emit('on-execute')" />
+                        @onClick="runWorkflowAndRedirect" />
                     <GButtonGroup v-else>
                         <GButton
                             :title="localize('Run Workflow')"
