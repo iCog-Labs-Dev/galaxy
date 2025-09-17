8b8108c73ede1a4e8ae17fe3a299498e0ec36e5a
johnnas12 <yohanesmesfin3@gmail.com>
2025-09-17 15:43:09 +0300

    fix: typo in style

--- client/src/components/ActivityBar/ActivityBar.vue
+++ client/src/components/ActivityBar/ActivityBar.vue
@@ -288,7 +288,7 @@ defineExpose({
                     height="56"
                     />
             </div>
-            <b-nav vertical class="flex-nowrap h-100 vertical-overflow" style="margin-top: 24pxx; align-items: center;">
+            <b-nav vertical class="flex-nowrap h-100 vertical-overflow" style="margin-top: 24px; align-items: center;">
                 <draggable
                     v-model="activities"
                     :class="{ 'activity-popper-disabled': isDragging }"
